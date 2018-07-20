105710de854c495a1e5fe830f85fca6f5a2df1a4
JanMach97 <janmachacek97@gmail.com>
2018-07-20 14:54:20 +0100

    basic game loop logic added

--- game_logic/game.js
+++ game_logic/game.js
@@ -17,15 +17,18 @@ const bulbasaur = new Pokemon(
   'raizor leaf',
   'grass'
 );
-const squirtle = new Pokemon('squirtle', 95, 23, 'SQUIRTLE', 'splash', 'water');
+const squirtle = new Pokemon('squirtle', 100, 23, 'SQUIRTLE', 'splash', 'water');
 const mewtwo = new Pokemon('mewtwo', 150, 40, 'hello', 'confusion', 'psychic');
 const ash = new Trainer('Ash', [charmeleon, bulbasaur]);
 const gary = new Trainer('Gary', [squirtle, mewtwo]);
 
-console.log('Welcome to Pokemon North!');
+let P1 = null;
+let P2 = null;
+let T1 = null;
+let T2 = null;
 
-let trainerChoice = null;
 
+/////QUESTIONS//////
 let question1 = [
   {
     type: 'list',
@@ -34,40 +37,114 @@ let question1 = [
     choices: [ash.name, gary.name]
   }
 ]
+let activity = [
+  {
+    type: 'list',
+    name: 'activity',
+    message: 'What would you like to do?',
+    choices: ['battle', 'catch pokemon']
+  }
+]
+let ashQuestion = [
+  {
+    type: 'list',
+    name: 'choosePokemon',
+    message: '',
+    choices: [ash.collection[0], ash.collection[1]]
+  }
+]
+let garyQuestion = [
+  {
+    type: 'list',
+    name: 'choosePokemon',
+    message: ``,
+    choices: [gary.collection[0], gary.collection[1]]
+  }
+]
+let battleOptions = [
+  {
+    type: 'list',
+    name: 'battleOptions',
+    message: '',
+    choices: ['attack', 'swap pokemon']
+  }
+]
+
+
+///////////
+
+console.log('Welcome to Pokemon North!');
+
+
+
 inquirer.prompt(question1).then(answers => {
-  trainerChoice = answers.chooseTrainer;
+  console.log(`you chose ${answers.chooseTrainer}`)
+  if (answers.chooseTrainer === 'Ash') {
+    T1 = ash;
+    T2 = gary;
+  } else {
+    T1 = gary;
+    T2 = ash;
+  }
+
+  function adventure() {
+    inquirer.prompt(activity).then(answer => {
+      if (answer.activity === 'battle') {
+
+        if (answers.chooseTrainer === 'Ash') {
+          inquirer.prompt(ashQuestion).then(answer => {
+            for (let i = 0; i < ash.collection.length; i++) {
+              if (T1.collection[i].name === answer.choosePokemon) {
+                P1 = T1.collection[i];
+                P2 = T2.collection[0];
+              }
+            }
+            console.log(`You chose ${answer.choosePokemon}. Your rival chose ${P2.name}. Let's battle!!!`);
+            const newBattle = new Battle(T1, T2, P1, P2);
+
+            function battleOption() {
+              inquirer.prompt(battleOptions).then(answer => {
+                if (answer.battleOptions === 'attack') {
+                  console.log(newBattle.fight());
+                  console.log(newBattle.fight());
+                  if (P2.health > 0 && P1.health > 0) {
+                    return battleOption();
+                  } else {
+                    return adventure();
+                  }
+                }
+              })
+            }
+            battleOption();
+
+
+
+          })
+        } else {
+          inquirer.prompt(garyQuestion).then(answer => {
+            for (let i = 0; i < gary.collection.length; i++) {
+              if (gary.collection[i].name === answer.choosePokemon) {
+                P1 = gary.collection[i];
+                P2 = ash.collection[0];
+              }
+            }
+            console.log(`You chose ${answer.choosePokemon}. Your rival chose ${P2.name}. Let's battle!!!`);
+            const newBattle = new Battle(T1, T2, P1, P2)
+            inquirer.prompt(battleOptions).then(answer => {
+              if (answer.battleOptions === 'attack') {
+                console.log('Fight me');
+              }
+            })
+          })
+        }
+      }
+    })
+  }
+  adventure();
+
+
+
+
+
 });
 
-// let question2 = [
-//   {
-//     type: 'list',
-//     name: 'choosePokemon',
-//     message: `You chose ${trainerChoice}.`,
-//     choices: [ash.name, gary.name]
-//   }
-// ];
-
-// inquirer.prompt(question2).then(answers => {
-//   // trainerChoice = answers.chooseTrainer;
-//   console.log(answers);
-// });
-
-// let question2 = [
-//   {
-//     type: 'list',
-//     name: 'choosePokemon',
-//     message: `You chose ${answers.chooseTrainer}.`,
-//     // message: 'Gary wants to battle! Choose your Pokemon...',
-//   }
-//   {
-//     type: 'list',
-//     name: 'choosePokemon',
-//     // message: `You chose ${this.answers}.`,
-//     message: 'Gary wants to battle! Choose your Pokemon...',
-//     choices: [charmeleon.name, bulbasaur.name]
-//   }
-// ];
-
-// inquirer.prompt(questions).then(answers => {
-//   console.log(answers);
-// });
--- pokemon-battle.js
+++ pokemon-battle.js
@@ -1,64 +1,74 @@
-function Trainer(name, collection) {
-  this.name = name;
-  this.collection = collection;
-}
-
-function Pokemon(name, health, attackDmg, sound, move, type = null) {
-  this.name = name;
-  this.health = health;
-  this.attackDmg = attackDmg;
-  this.sound = sound;
-  this.move = move;
-  this.type = type;
-  if (this.type === 'fire') {
-    this.strongAgainst = 'grass';
-    this.weakagainst = 'water';
-  } else if (this.type === 'water') {
-    this.strongAgainst = 'fire';
-    this.weakagainst = 'grass';
-  } else if (this.type === 'grass') {
-    this.strongAgainst = 'water';
-    this.weakagainst = 'fire';
-  } else {
-    this.type = null;
+class Trainer {
+  constructor(name, collection) {
+    this.name = name;
+    this.collection = collection;
+  }
+  catch(pokemon) {
+    this.collection.push(pokemon);
   }
 }
 
-function Battle(trainer1, trainer2, pokemon1, pokemon2) {
-  this.trainer1 = trainer1;
-  this.pokemon1 = pokemon1;
-  this.trainer2 = trainer2;
-  this.pokemon2 = pokemon2;
-  this.turn = pokemon1;
-  this.notTurn = pokemon2;
-}
-
-Trainer.prototype.catch = function (pokemon) {
-  this.collection.push(pokemon);
-}
-
-Pokemon.prototype.talk = function () {
-  return this.sound;
-}
-
-Pokemon.prototype.useYourMove = function () {
-  return this.move;
-}
-
-Battle.prototype.fight = function () {
-  if (this.turn.strongAgainst === this.notTurn.type) this.turn.attackDmg *= 1.25;
-  if (this.turn.weakagainst === this.notTurn.type) this.turn.attackDmg *= 0.75;
-
-  if (this.notTurn.health - this.turn.attackDmg <= 0) {
-    return `${this.turn.name} attacked ${this.notTurn.name} with ${this.turn.move}. ${this.notTurn.name} has fainted. ${this.turn.name} wins`;
+class Pokemon {
+  constructor(name, health, attackDmg, sound, move, type = null) {
+    this.name = name;
+    this.health = health;
+    this.attackDmg = attackDmg;
+    this.sound = sound;
+    this.move = move;
+    this.type = type;
+    if (this.type === 'fire') {
+      this.strongAgainst = 'grass';
+      this.weakagainst = 'water';
+    } else if (this.type === 'water') {
+      this.strongAgainst = 'fire';
+      this.weakagainst = 'grass';
+    } else if (this.type === 'grass') {
+      this.strongAgainst = 'water';
+      this.weakagainst = 'fire';
+    } else {
+      this.type = null;
+    }
+  }
+  talk() {
+    return this.sound;
   }
 
-  this.notTurn.health -= this.turn.attackDmg
-
-  this.turn === this.pokemon1 ? this.turn = this.pokemon2 : this.turn = this.pokemon1;
-  this.notTurn === this.pokemon2 ? this.notTurn = this.pokemon1 : this.notTurn = this.pokemon2;
-
-  return `${this.notTurn.name} attacked ${this.turn.name} with ${this.notTurn.move}`
+  useYourMove() {
+    return this.move;
+  }
 }
+
+class Battle {
+  constructor(trainer1, trainer2, pokemon1, pokemon2) {
+    this.trainer1 = trainer1;
+    this.pokemon1 = pokemon1;
+    this.trainer2 = trainer2;
+    this.pokemon2 = pokemon2;
+    this.turn = pokemon1;
+    this.notTurn = pokemon2;
+  }
+  fight() {
+    if (this.turn.strongAgainst === this.notTurn.type) this.turn.attackDmg *= 1.25;
+    if (this.turn.weakagainst === this.notTurn.type) this.turn.attackDmg *= 0.75;
+
+    if (this.notTurn.health - this.turn.attackDmg <= 0) {
+      this.notTurn.health -= this.turn.attackDmg
+      return `${this.turn.name} attacked ${this.notTurn.name} with ${this.turn.move}. ${this.notTurn.name} has fainted. ${this.turn.name} wins`;
+    }
+
+    this.notTurn.health -= this.turn.attackDmg
+
+    this.turn === this.pokemon1 ? this.turn = this.pokemon2 : this.turn = this.pokemon1;
+    this.notTurn === this.pokemon2 ? this.notTurn = this.pokemon1 : this.notTurn = this.pokemon2;
+
+    return `${this.notTurn.name} attacked ${this.turn.name} with ${this.notTurn.move}. ${this.turn.name} lost ${this.notTurn.attackDmg} health. It only has ${this.turn.health} left`
+  }
+}
+
+
+
+
+
+
 
 module.exports = { Trainer, Pokemon, Battle }
